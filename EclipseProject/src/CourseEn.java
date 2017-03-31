77e7e2b5c54bc863bb3dc6284f19a1445b25f5ce
Shubham Munot <samunot@ncsu.edu>
2017-03-31 17:08:11 -0400

    tables for our er created

--- EclipseProject/src/CourseEn.java
+++ EclipseProject/src/CourseEn.java
@@ -1,9 +1,16 @@
-
+import java.util.*;
 public class CourseEn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static void login() {
+		System.out.println("Enter username");
+		Scanner sc = new Scanner(System.in);
+		String username = sc.nextLine();
+		
 	}
 
 }
--- sql_queries.sql
+++ sql_queries.sql
@@ -1,52 +1,159 @@
 
 //create table queries 
-create table fallcourse(
-	name varchar(50),
-	fallcourse_id varchar(20),
-	maxCredits int,
-	minCredits int,
-	department varchar(20),
-	gradLevel varchar(20),
-	classSize int,
-	waitlistSize int,
-	schedule varchar(100),
-	prerequisiteCourses varchar(50),
-	primary key (fallcourse_id)
+
+create table user(
+	firstName varchar(20) NOT NULL,
+	lastName varchar(20),
+	email varchar(40) NOT NULL UNIQUE,
+	password varchar(20) NOT NULL,
+	role int,
+	street varchar(50),
+	city varchar(20),
+	zip int,
+	state varchar(20),
+	user_id int,
+	primary key (user_id),
+	constraint role_check check (role>=0 AND role<=2)
 	);
 
-create table springcourse(
-	name varchar(50),
-	springcourse_id varchar(20),
-	maxCredits int,
-	minCredits int,
-	department varchar(20),
-	gradLevel varchar(20),
-	classSize int,
-	waitlistSize int,
-	schedule varchar(100),
-	prerequisiteCourses varchar(50),
-	primary key (springcourse_id)
+create table admin(
+	ssn int NOT NULL UNIQUE,
+	admin_id int,
+	user_id int,
+	foreign key (user_id) REFERENCES user,
+	primary key (admin_id)
 	);
 
 create table student(
-	name varchar(50),
+	dob date NOT NULL,
 	student_id int,
-	dob date,
-	status varchar(50),
-	gradLevel varchar(20),
-	department varchar(20),
-	billAmount int,
+	user_id int,
+	gpa float,
+	termEnrolled varchar(10),
+	residencyLevel int NOT NULL,
+	tuitionBill float,
+	gradLevel int NOT NULL,
+	hasGraduated boolean,
+	foreign key (user_id) REFERENCES user,
 	primary key (student_id)
 	);
 
-create table fallcoursegrade(
-	student_id int,
-	fallcourse_id varchar(20),
-	grade varchar(2),
-	foreign key (student_id) references student,
-	foreign key (fallcourse_id) references fallcourse,
-	primary key (student_id,fallcourse_id)
+create table faculty(
+	faculty_id int,
+	user_id int,
+	foreign key (user_id) REFERENCES user,
+	primary key (faculty_id)
 	);
+
+create table term(
+	year int,
+	semester varchar(10),
+	addDeadline date,
+	dropDeadline date,
+	primary key (year,semester)
+	);
+
+create table course(
+	title varchar(50) NOT NULL UNIQUE,
+	course_id varchar(20),
+	minGPA float,
+	gradLevel int NOT NULL,
+	credits int NOT NULL,
+	permission boolean DEFAULT FALSE,
+	primary key (course_id)
+	);
+
+create table offering(
+	course_id varchar(20),
+	year int,
+	semester varchar(10),
+	maxWaitist int,
+	location varchar(20),
+	maxSize int,
+	scheduleDay varchar(20),
+	scheduleTime time,
+	foreign key (course_id) REFERENCES course,
+	foreign key (year) REFERENCES term,
+	foreign key (semester) REFERENCES term,
+	primary key (course_id,year,semester)
+	);
+
+create table department(
+	department_id varchar(20),
+	name varchar(40),
+	primary key (department_id)
+	);
+
+create table offers(
+	department_id varchar(20),
+	course_id varchar(20),
+	foreign key (department_id) REFERENCES department,
+	foreign key (course_id) REFERENCES course,
+	primary key (department_id,course_id)
+	);
+ 
+create table studentDepartment(
+	department_id varchar(20),
+	student_id int,
+	foreign key (department_id) REFERENCES department,
+	foreign key (student_id) REFERENCES student,
+	primary key (department_id,student_id)
+	);
+
+create table prereq(
+	coursePrereq_id varchar(20),
+	course_id varchar(20),
+	minGrade varchar(2),
+	foreign key (coursePrereq_id) REFERENCES course,
+	foreign key (course_id) REFERENCES course,
+	primary key (coursePrereq_id,course_id)
+	);
+
+create table specperm(
+	student_id int,
+	course_id varchar(20),
+	admin_id int,
+	foreign key (student_id) REFERENCES student,
+	foreign key (admin_id) REFERENCES admin,
+	foreign key (course_id) REFERENCES course,
+	primary key (student_id,course_id,admin_id)
+	);
+
+create table createCourse(
+	admin_id int,
+	course_id varchar(20),
+	foreign key (admin_id) REFERENCES admin,
+	foreign key (course_id) REFERENCES course,
+	primary key (admin_id,course_id)
+	);
+
+create table teaches(
+	faculty_id int,
+	year int,
+	semester varchar(10)
+	course_id varchar(20),
+	foreign key (faculty_id) REFERENCES faculty,
+	foreign key (year) REFERENCES term,
+	foreign key (semester) REFERENCES term,
+	foreign key (course_id) REFERENCES course,
+	primary key (faculty_id,year,semester,course_id)
+	);
+
+create table enrolled(
+	student_id int,
+	course_id varchar(20),
+	year int,
+	semester varchar(10),
+	grade varchar(2),
+	waitlistNumber int,
+	enrolledStatus boolean,
+	foreign key (student_id) REFERENCES student,
+	foreign key (year) REFERENCES offering,
+	foreign key (semester) REFERENCES offering,
+	foreign key (course_id) REFERENCES offering,
+	primary key (student_id,year,semester,course_id)
+	);
+
 
 Create
 //insert in 
