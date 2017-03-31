80d455b9257270725170abcd5b4e79b2300c18d0
Rishi <rjain9@ncsu.edu>
2017-03-31 14:49:10 -0400

    Adding login sample. Follow that

---
+++ EclipseProject/src/DatabaseHandler.java
@@ -0,0 +1,8 @@
+
+public class DatabaseHandler {
+	
+	public int loginValidate(String username, String password){
+		//Insert code for db hit to verify user as admin or student and return to the controller		
+		return 1;//1 if student 2 if admin 3 if invalid
+	}
+}
