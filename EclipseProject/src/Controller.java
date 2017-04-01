9f09a26d738442e67cf0ad200c6f766147b7a56d
Rishi <rjain9@ncsu.edu>
2017-04-01 14:10:30 -0400

    Other finctions

--- EclipseProject/src/Controller.java
+++ EclipseProject/src/Controller.java
@@ -3,30 +3,59 @@ import java.util.Scanner;
 
 public class Controller {
 	
+	static Scanner sc;
 	static DatabaseHandler dbhandler;
+	static String username;
+	
 	
 	public static void main(String[] arg)throws SQLException{
 		dbhandler = new DatabaseHandler();
 		int isStudent = login();
 		if(isStudent==1){
-			//call student functions here
+			studentFunctions();
 		}else if (isStudent==2){
-			//call admin functions here
+			adminFunctions();
 		}else{
-			System.exit(0);
+			System.out.println("Invalid username or password. Please try again!");
+			login();
 		}
 	}
 	
 	private static int login()throws SQLException{
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		System.out.print("Enter username: ");
-		String username = sc.next();
+		username = sc.next();
 		System.out.print("\nEnter Password: ");
 		String password = sc.next();
-		
 		int isStudent = dbhandler.loginValidate(username, password);
+		return isStudent;	
+	}
+	
+	private static void studentFunctions(){
+		System.out.println("1.Enroll courses\n2.Drop courses\n");
+		int choice  = sc.nextInt();
+		switch (choice) {
+		case 1: enrollCourse();
+			break;
+			
+		case 2: dropCourse();
+			break;
+			
+		case 3: 
+		default:
+			break;
+		}
+	}
+	
+	private static void adminFunctions(){
 		
-		return isStudent;
+	}
+	
+	private static void enrollCourse(){
+		
+	}
+	
+	private static void dropCourse(){
 		
 	}
 }
