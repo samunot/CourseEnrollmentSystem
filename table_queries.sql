df7573ebfbdd84009419f0d9ca6f0521c53bdada
asaxena3 <asaxena3@ncsu.edu>
2017-04-02 13:06:56 -0400

    modifications of saturday

--- table_queries.sql
+++ table_queries.sql
@@ -3,25 +3,25 @@
 create table users(
 	firstName varchar(20) NOT NULL,
 	lastName varchar(20),
-	username varchar(40) NOT NULL UNIQUE,
+	username varchar(20),
 	password varchar(20) NOT NULL,
 	role varchar(20) NOT NULL,
 	dob date NOT NULL,
+	phone int,
 	street varchar(50),
 	city varchar(20),
 	zip int,
 	state varchar(20),
 	primary key (username),
 	constraint zip_check check (zip>=0 AND zip<100000),
-	constraint role_check check(role like 'student|admin|faculty')
+	constraint role_check check(role in ('student','admin','faculty'))
 	);
 
 create table admin(
-	employee_id int,
-	username varchar(20)
+	username varchar(20),
 	ssn int NOT NULL UNIQUE,
 	foreign key (username) REFERENCES users,
-	primary key (employee_id)
+	primary key (username)
 	);
 
 create table student(
@@ -29,21 +29,23 @@ create table student(
 	username varchar(20),
 	gpa float,
 	termEnrolled varchar(10),
-	residencyLevel varchar(20) NOT NULL,
+	residencyLevel int NOT NULL,
 	tuitionBill float,
 	tuitionOwed float,
-	phone int NOT NULL,
+            minCredit int ,
+	maxCredit int,
 	gradLevel int NOT NULL,
 	hasGraduated char(1) NOT NULL,
 	email varchar(40) NOT NULL,
 	foreign key (username) REFERENCES users,
 	primary key (student_id),
-	constraint hasGraduated_check check (hasGraduated like '[YyNn]')
+	constraint hasGraduated_check check (hasGraduated in ('Y','N')),
+	constraint level_check check(residencyLevel in ('international','in state','out of state'))
 	);
 
 create table faculty(
 	faculty_id int,
-	username int,
+	username varchar(20),
 	foreign key (username) REFERENCES users,
 	primary key (faculty_id)
 	);
@@ -60,24 +62,23 @@ create table semester(
 create table course(
 	title varchar(50) NOT NULL UNIQUE,
 	course_id varchar(20),
-	employee_id int,
+            department_id varchar(5),
 	gradLevel int NOT NULL,
 	credits int NOT NULL,
 	permission char(1) NOT NULL,
 	primary key (course_id),
-	foreign key (employee_id) REFERENCES admin,
+	foreign key (deoartment_id) REFERENCES department,
 	constraint perm_check check (permission like '[YyNn]')
 	);
 
 create table department(
-	department_id varchar(20),
+	department_id varchar(5),
 	name varchar(40),
 	primary key (department_id)
 	);
 
 create table offering(
 	course_id varchar(20),
---	department_id varchar(20),
 	faculty_id int,
 	sem varchar(3),
 	maxWaitist int,
@@ -89,7 +90,6 @@ create table offering(
 	scheduleTime varchar(20),
 	foreign key (faculty_id) REFERENCES faculty,
 	foreign key (course_id) REFERENCES course,
---	foreign key (department_id) REFERENCES department,
 	foreign key (sem) REFERENCES semester,
 	primary key (course_id,sem),
 	constraint waitlist_check check(maxWaitist>=studentsWaitlisted),
@@ -125,24 +125,16 @@ create table prereq(
 create table specialPermission(
 	student_id int,
 	course_id varchar(20),
-	employee_id int,
+	username int,
 	sem varchar(3),
 	reqDate date,
 	approveDate date,
 	foreign key (student_id) REFERENCES student,
-	foreign key (employee_id) REFERENCES admin,
+	foreign key (username) REFERENCES admin,
 	foreign key (course_id) REFERENCES course,
 	foreign key (sem) REFERENCES semester,
 	primary key (student_id,course_id,sem)
 	);
-
---create table createCourse(
---	employee_id int,
---	course_id varchar(20),
---	foreign key (employee_id) REFERENCES admin,
---	foreign key (course_id) REFERENCES course,
---	primary key (employee_id,course_id)
---	);
 
 create table enrolled(
 	student_id int,
